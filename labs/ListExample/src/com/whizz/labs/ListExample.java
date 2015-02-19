3299ffc7851604611759e4cf30614622360064f1
krishna.kv <krishna.kv@flipkart.com>
2015-02-19 14:38:25 +0530

    first lab

---
+++ labs/ListExample/src/com/whizz/labs/ListExample.java
@@ -0,0 +1,18 @@
+package com.whizz.labs;
+
+// TODO: Include required packages
+
+public class ListExample {
+	
+	public static void main(String[] args) {
+		
+		// TODO 1.: Add a List<String>
+		
+		list.add("Krishna");
+		// TODO 2 :Insert 2 more strings
+		
+		// TODO 3: Print size of list
+		
+		// TODO 4 : loop through all the elements
+	}
+}
